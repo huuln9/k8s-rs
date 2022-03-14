3347101d08fbe8b39ff3db9aadb3719601532c5a
Lý Ngọc Hữu <huuln9@gmail.com>
2022-03-14 16:02:21 +0700

    change src

--- lib/main.dart
+++ lib/main.dart
@@ -48,68 +48,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  bool english = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: const Image(
+            image: NetworkImage(
+                'https://raw.githubusercontent.com/huuln9/images/main/banner_2.png'),
+            fit: BoxFit.cover,
+          ),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            onPressed: () => {},
+          ),
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Setting",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            ListTile(
+              leading: const Icon(Icons.language, color: Colors.black),
+              title: const Text(
+                "English",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Padding(
+                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
+                child: IconButton(
+                  icon: english
+                      ? const Icon(
+                          Icons.toggle_on,
+                          size: 50,
+                          color: Colors.green,
+                        )
+                      : const Icon(
+                          Icons.toggle_off,
+                          size: 50,
+                          color: Colors.black54,
+                        ),
+                  onPressed: () {
+                    setState(() {
+                      english = !english;
+                    });
+                    if (english) {
+                      // Get.updateLocale(const Locale('en', 'US'));
+                    } else {
+                      // Get.updateLocale(const Locale('vi', 'VN'));
+                    }
+                  },
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Divider(thickness: 1.0),
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications, color: Colors.black),
+              title: const Text(
+                "Enable notifications",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Padding(
+                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.toggle_off,
+                    size: 50,
+                    color: Colors.black54,
+                  ),
+                  onPressed: () => {},
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Divider(thickness: 1.0),
+            ),
+            const ListTile(
+              leading: Icon(Icons.message, color: Colors.black),
+              title: Text(
+                "Comment",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Divider(thickness: 1.0),
+            ),
+            const ListTile(
+              leading: Icon(Icons.phone_android, color: Colors.black),
+              title: Text(
+                "Version",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Text(
+                "1.0.0",
+                style: TextStyle(fontSize: 18, color: Colors.blue),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: Divider(thickness: 1.0),
+            ),
+            const ListTile(
+              leading: Icon(
+                Icons.ondemand_video,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Instructional clips",
+                style: TextStyle(fontSize: 18),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
