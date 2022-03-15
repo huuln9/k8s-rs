622e8d74ada7bf17174b40a42b97d84db87fc613
Lý Ngọc Hữu <huuln9@gmail.com>
2022-03-15 09:44:15 +0700

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -48,29 +48,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool english = false;
+  bool isSwitched = false;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.grey.shade100,
         appBar: AppBar(
-          flexibleSpace: const Image(
-            image: NetworkImage(
-                'https://raw.githubusercontent.com/huuln9/images/main/banner_2.png'),
-            fit: BoxFit.cover,
-          ),
           leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-            ),
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
             onPressed: () => {},
           ),
           title: const Align(
             alignment: Alignment.centerLeft,
             child: Text(
-              "Setting",
+              "Cài đặt ứng dụng",
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
           ),
@@ -79,100 +72,119 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(padding: EdgeInsets.only(top: 10)),
-            ListTile(
-              leading: const Icon(Icons.language, color: Colors.black),
-              title: const Text(
-                "English",
-                style: TextStyle(fontSize: 18),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                border: Border(bottom: BorderSide(color: Colors.black12)),
               ),
-              trailing: Padding(
-                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                child: IconButton(
-                  icon: english
-                      ? const Icon(
-                          Icons.toggle_on,
-                          size: 50,
-                          color: Colors.green,
-                        )
-                      : const Icon(
-                          Icons.toggle_off,
-                          size: 50,
-                          color: Colors.black54,
-                        ),
-                  onPressed: () {
-                    setState(() {
-                      english = !english;
-                    });
-                    if (english) {
-                      // Get.updateLocale(const Locale('en', 'US'));
-                    } else {
-                      // Get.updateLocale(const Locale('vi', 'VN'));
-                    }
-                  },
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            ListTile(
-              leading: const Icon(Icons.notifications, color: Colors.black),
-              title: const Text(
-                "Enable notifications",
-                style: TextStyle(fontSize: 18),
-              ),
-              trailing: Padding(
-                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.toggle_off,
-                    size: 50,
-                    color: Colors.black54,
+              child: ListTile(
+                leading: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black12,
                   ),
-                  onPressed: () => {},
+                  child: const Icon(
+                    Icons.notifications_none,
+                    color: Colors.black54,
+                    size: 30,
+                  ),
+                ),
+                title: const Text(
+                  "Nhận thông báo",
+                  style: TextStyle(fontSize: 18),
+                ),
+                trailing: Switch(
+                  value: isSwitched,
+                  onChanged: (value) {
+                    setState(() {
+                      isSwitched = value;
+                    });
+                  },
+                  activeTrackColor: Colors.black12,
+                  activeColor: Colors.blue,
+                  inactiveTrackColor: Colors.black12,
                 ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            const ListTile(
-              leading: Icon(Icons.message, color: Colors.black),
-              title: Text(
-                "Comment",
-                style: TextStyle(fontSize: 18),
+            // const Divider(thickness: 1.0),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                border: Border(bottom: BorderSide(color: Colors.black12)),
+              ),
+              child: ListTile(
+                leading: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black12,
+                  ),
+                  child: const Icon(
+                    Icons.amp_stories_outlined,
+                    color: Colors.black54,
+                    size: 30,
+                  ),
+                ),
+                title: const Text(
+                  "Xem hướng dẫn",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            const ListTile(
-              leading: Icon(Icons.phone_android, color: Colors.black),
-              title: Text(
-                "Version",
-                style: TextStyle(fontSize: 18),
+
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                border: Border(bottom: BorderSide(color: Colors.black12)),
               ),
-              trailing: Text(
-                "1.0.0",
-                style: TextStyle(fontSize: 18, color: Colors.blue),
+              child: ListTile(
+                leading: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black12,
+                  ),
+                  child: const Icon(
+                    Icons.share,
+                    color: Colors.black54,
+                    size: 30,
+                  ),
+                ),
+                title: const Text(
+                  "Chia sẻ ứng dụng",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            const ListTile(
-              leading: Icon(
-                Icons.ondemand_video,
-                color: Colors.black,
-              ),
-              title: Text(
-                "Instructional clips",
-                style: TextStyle(fontSize: 18),
+
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              decoration: const BoxDecoration(color: Colors.white),
+              child: ListTile(
+                leading: Container(
+                  padding: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black12,
+                  ),
+                  child: const Icon(
+                    Icons.mobile_friendly,
+                    color: Colors.black54,
+                    size: 30,
+                  ),
+                ),
+                title: const Text(
+                  "Phiên bản ứng dụng",
+                  style: TextStyle(fontSize: 18),
+                ),
+                trailing: const Text(
+                  "1.2.0",
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
             ),
           ],
